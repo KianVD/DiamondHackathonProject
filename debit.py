8e08402e31d807e03b804f5f0b08934242e46a76
KianVD <kdrees@sandiego.edu>
2025-04-05 14:30:37 -0700

    read in csv

--- debit.py
+++ debit.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import pandas as pd
+
+df = pd.read_csv('MOCK_DATA.csv')
+print(df.columns)
+df = df[['transaction_date','transaction_amount','transaction_type','transaction_category','amount_before','amount_after','user']]
+df = pd.get_dummies(df, columns=['transaction_type'])
+print(df.head())
